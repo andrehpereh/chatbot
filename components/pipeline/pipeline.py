def98e905f8260dbef4bf515eb056124993fb837
andrehpereh <andrehpereh@gmail.com>
2024-03-31 00:32:39 +0000

    chatbot-0033: Arreglando mi pendejada

--- components/pipeline/pipeline.py
+++ components/pipeline/pipeline.py
@@ -7,7 +7,7 @@ from util import get_model_paths_and_config
 from google.cloud import aiplatform as vertexai
 import os
 
-
+TAG_NAME = os.environ.get('TAG_NAME', 'latest') 
 @dsl.component(base_image='python:3.9')
 def send_pipeline_completion_email_op(
     status: PipelineTaskFinalStatus,
@@ -53,7 +53,7 @@ def send_pipeline_completion_email_op(
 
 
 @dsl.component(
-  base_image ='gcr.io/able-analyst-416817/gemma-chatbot-data-preparation:latest'
+    base_image =f"gcr.io/able-analyst-416817/gemma-chatbot-data-preparation:{TAG_NAME}"
 )
 def process_whatsapp_chat_op(
   bucket_name: str,
@@ -68,14 +68,15 @@ def process_whatsapp_chat_op(
 
 
 @dsl.component(
-  base_image = 'gcr.io/able-analyst-416817/gemma-chatbot-fine-tunning:latest'
+    base_image = f"gcr.io/able-analyst-416817/gemma-chatbot-fine-tunning:{TAG_NAME}"
 )
 def fine_tunning(
   dataset_path: InputPath('Dataset'),
   model_paths: dict,
   fine_tune_flag: bool,
   epochs: int,
-  model_name: str
+  model_name: str,
+  bucket_name: str
 ) -> str:
     import trainer
     import json
@@ -88,7 +89,6 @@ def fine_tunning(
     
     model = trainer.finetune_gemma(dataset, model_paths, fine_tune_flag, epochs=epochs, model_name=model_name)
     print("Its gonna save it here", finetuned_weights_path)
-    bucket_name = 'able-analyst-416817-chatbot-v1' # move to parameter.
     util.upload2bs(
         local_directory = model_paths['finetuned_model_dir'], bucket_name = bucket_name,
         destination_subfolder = model_paths['fine_tuned_keras_blob']
@@ -99,16 +99,16 @@ def fine_tunning(
     
 
 @dsl.component(
-  base_image = 'gcr.io/able-analyst-416817/gemma-chatbot-fine-tunning:latest'
+    base_image = f"gcr.io/able-analyst-416817/gemma-chatbot-fine-tunning:{TAG_NAME}"
 )
 def convert_checkpoints_op(
   keras_gcs_model: str,
-  model_paths: dict
+  model_paths: dict,
+  bucket_name: str
 ) -> str:
     import conversion_function
     import os
     import util
-    bucket_name, blob_name = os.path.dirname(keras_gcs_model).lstrip("gs://").split("/", 1) 
     print("This is the keras passed", keras_gcs_model)
     util.download_all_from_blob(bucket_name, model_paths['fine_tuned_keras_blob'], local_destination=model_paths['finetuned_model_dir'])
     if os.path.exists("./model.weights.h5"):
@@ -128,7 +128,7 @@ def convert_checkpoints_op(
     return model_paths['deployed_model_uri']
 
 
-@kfp.dsl.pipeline(name="Model deployment.")
+@dsl.pipeline(name="Model deployment.")
 def fine_tune_pipeline(
     project: str = os.environ.get('PROJECT_ID') ,
     bucket_name: str = "able-analyst-416817-chatbot-v1",
@@ -142,7 +142,6 @@ def fine_tune_pipeline(
     from google_cloud_pipeline_components.types import artifact_types
     from google_cloud_pipeline_components.v1.endpoint import (EndpointCreateOp, ModelDeployOp)
     from google_cloud_pipeline_components.v1.model import ModelUploadOp
-    from google_cloud_pipeline_components.v1.vertex_notification_email import VertexNotificationEmailOp
     from kfp.dsl import importer_node
     from util import get_model_paths_and_config
     from config import Config
@@ -180,12 +179,15 @@ def fine_tune_pipeline(
     with ExitHandler(send_email):
         whatup = process_whatsapp_chat_op(bucket_name = bucket_name, directory = directory)
 
-        trainer = fine_tunning(dataset_path=whatup.outputs['dataset_path'], model_paths=model_paths, fine_tune_flag=fine_tune_flag, epochs=epochs, model_name=model_name)
+        trainer = fine_tunning(
+            dataset_path=whatup.outputs['dataset_path'], model_paths=model_paths, fine_tune_flag=fine_tune_flag,
+            epochs=epochs, model_name=model_name, bucket_name = bucket_name
+        )
         trainer.set_memory_limit("50G").set_cpu_limit("12.0").set_accelerator_limit(1).add_node_selector_constraint(model_paths['accelerator_type'])
 
         print("This is the dictionary", model_paths)
         converted = convert_checkpoints_op(
-            keras_gcs_model=trainer.output, model_paths=model_paths
+            keras_gcs_model=trainer.output, model_paths=model_paths, bucket_name = bucket_name
         ).set_memory_limit("50G").set_cpu_limit("12.0").set_accelerator_limit(1).add_node_selector_constraint(model_paths['accelerator_type'])
 
         import_unmanaged_model_task = importer_node.importer(
@@ -198,20 +200,20 @@ def fine_tune_pipeline(
 
         model_upload_op = ModelUploadOp(
             project=project,
-            display_name="Mini Andres, first version automated",
+            display_name=f"Mini {Config.USER_NAME} model; first version automated",
             unmanaged_container_model=import_unmanaged_model_task.outputs["artifact"],
         )
         model_upload_op.after(import_unmanaged_model_task)
 
         endpoint_create_op = EndpointCreateOp(
             project=project,
-            display_name="model_deployment_pipeline: End Point Created",
+            display_name=f"End point created for {Config.USER_NAME}: End Point Created",
         )
 
         ModelDeployOp(
             endpoint=endpoint_create_op.outputs["endpoint"],
             model=model_upload_op.outputs["model"],
-            deployed_model_display_name=model_paths['model_name_vllm'],
+            deployed_model_display_name=f"Model {ModelDeployOp}_{model_paths['model_name_vllm']}",
             dedicated_resources_machine_type=model_paths['machine_type'],
             dedicated_resources_min_replica_count=1,
             dedicated_resources_max_replica_count=1,
@@ -221,18 +223,22 @@ def fine_tune_pipeline(
 
 
 if __name__ == '__main__':
-
+    from kfp import compiler
+    from google.cloud import aiplatform as vertexai
     from config import Config
-    from util import get_model_paths_and_config
-    
-    model_paths = get_model_paths_and_config(Config.MODEL_NAME)
+    print("This is the model name", Config.MODEL_NAME, "Ahuevito")
+    print("This is the directory", Config.TRAIN_DATA_DIR, "Ahuevito")
+    print("This is the BUCKET_NAME", Config.BUCKET_NAME, "Ahuevito")
+    print("This is the FINE_TUNE_FLAG", Config.FINE_TUNE_FLAG, "Ahuevito")
+    print("This is the EPOCHS", Config.EPOCHS, "Ahuevito")
+    pipeline_name = f"fine_tune_pipeline{Config.USER_NAME}.json"
     compiler.Compiler().compile(
-        pipeline_func=fine_tune_pipeline, package_path="fine_tune_pipeline.json"
+        pipeline_func=fine_tune_pipeline, package_path=pipeline_name
     )
     vertexai.init(project=Config.PROJECT_ID, location=Config.REGION)
     vertex_pipelines_job = vertexai.pipeline_jobs.PipelineJob(
         display_name="test-fine_tune_pipeline",
-        template_path="fine_tune_pipeline.json",
+        template_path=pipeline_name,
         parameter_values={
             "project": Config.PROJECT_ID,
             "bucket_name": Config.BUCKET_NAME,
